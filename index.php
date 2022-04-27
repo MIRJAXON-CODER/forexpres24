0cc36ade2f2e0dbbb10b5dd2f394dabcaf1428f1
MIRJAXON-CODER <52111895+MIRJAXON-CODER@users.noreply.github.com>
2022-04-27 16:22:55 +0500

    For Express24 Task

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+	
+
+	class forTask
+	{
+	    static function getNum($arr)
+	    {
+	    	$number='';
+			foreach($arr as $key => $value){
+				$number.=$value;
+			}
+			return  intval(strrev($number));
+	    }
+
+	    static function getArray($number)
+	    {
+	    	$arr=[];
+	    	$number = strrev($number);
+	    	$xs = strlen($number);
+			for ($i=0; $i <$xs ; $i++){
+				$arr+=["$i" => $number[$i]];
+			}
+			return $arr;
+	    }
+	}
+	
+
+
+
+
+	$arr1 = [1,2,3,4,5];
+	$arr2 = [1,2,3,4,5];
+	$result = forTask::getNum($arr1)+forTask::getNum($arr1);
+	$newArr = (forTask::getArray($result));
+	print_r($newArr);
+	
+ ?>
